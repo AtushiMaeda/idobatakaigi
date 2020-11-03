f119e99677e1d94b572ef790df603dcb1a8455fe
AtsushiMaeda <tachidori@gmail.com>
2020-11-03 12:27:21 +0900

    create app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { TextField } from '@material-ui/core'
+import { TextField } from '@material-ui/core';
+import { pushMessage } from '../firebase'
 
 const MessageField = ({ name, setText, text }) => {
   const [isComposed, setIsComposed] = useState(false);
-  console.log({text});
 
   return (
     <TextField
@@ -15,7 +15,7 @@ const MessageField = ({ name, setText, text }) => {
         const text = e.target.value;
         if (text === '') return;
         if (e.key === 'Enter') {
-          console.log('push message to firebase');
+          pushMessage({ name: 'はむさん', text });
           setText('');
           e.preventDefault();
         }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm8vM2xECenLli-Fe_v8JaQKNJ6hD9cVo",
+  authDomain: "idobatakaigi-with-atsu.firebaseapp.com",
+  databaseURL: "https://idobatakaigi-with-atsu.firebaseio.com",
+  projectId: "idobatakaigi-with-atsu",
+  storageBucket: "idobatakaigi-with-atsu.appspot.com",
+  messagingSenderId: "864632316710",
+  appId: "1:864632316710:web:9539afe41d87cfa8fad7e7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
